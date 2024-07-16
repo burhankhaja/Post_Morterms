ce3ecfff627a95f4b0f7b722cf87461047269472
Burhan khaja <118989617+burhankhaja@users.noreply.github.com>
2024-07-16 21:30:57 +0530

    Create test.sol

---
+++ POCs/github_imports/test.sol
@@ -0,0 +1,9 @@
+// testing if you can test contracts in foundry that are imported with github link
+// if yes := very useful for audit contests for the minimal and flexible POC 
+/**
+*@dev import this via github link in your foundry test and call number function
+*don't forget to create an interface with number first;
+*/
+contract ImportMe {
+uint256 public number = 33;
+}
